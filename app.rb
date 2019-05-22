e3b5da911881261a61c14a0ccd0a8842f6f7b414
takaya <s.takaya1027@gmail.com>
2019-05-22 16:11:46 +0000

    update gemfile

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'bundler'
 Bundler.require
+require "sinatra/activerecord"
+
+set :database, {adapter: "sqlite3", database: "content.sqlite3"}
 
 get '/' do
     erb :home
@@ -8,4 +11,8 @@ end
 
 get '/form_new' do
    erb :form_new
+end
+
+post '/create_post' do
+   redirect '/'
 end
